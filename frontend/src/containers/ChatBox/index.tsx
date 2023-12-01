ac87971f265b8a1c25a75be0e46560099c43fbed
ubaidrmn <rehmanubaid2003@gmail.com>
2023-12-01 15:57:57 +0500

    feat: create chatbox component with fake test data

---
+++ frontend/src/containers/ChatBox/index.tsx
@@ -0,0 +1,89 @@
+import { styled } from "styled-components"
+
+const SentMessageBoxStyled = styled.div<{align: string}>`
+    div.main-container {
+        max-width: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: ${props => props.align};
+    }
+
+    div.message-box {
+        background-color: var(--green-dark);
+        color: white;
+        max-width: 300px;
+        width: 100%;
+        border-radius: 10px;
+        padding: 15px;
+        margin-bottom: 15px;
+    }
+`;
+
+const SendMessageInputStyled = styled.input`
+    border: 1px solid #d9d9d9;
+    width: 100%;
+    padding: 15px;
+    border-radius: 10px;
+    outline: none;
+    box-sizing: border-box;
+    &::placeholder {
+        color: #9dafbd;
+    }
+`;
+
+const ReceivedMessageBoxStyled = styled.div`
+    background-color: var(--grey-light);
+    color: var(--blue-dark);
+    max-width: 300px;
+    border-radius: 10px;
+    width: 100%;
+    padding: 15px;
+`;
+
+const ChatBox = () => {
+    const username = "ubaidrmn"
+
+    const messageHistory = [
+        {sender: "taha_zuberi", text: "Hello how are you doing!"},
+        {sender: "ubaidrmn", text: "Hello whatz upzzz ggg"},
+        {sender: "taha_zuberi", text: "Hello how are you doing!"},
+        {sender: "ubaidrmn", text: "Hello whatz upzzz ggg"},
+        {sender: "taha_zuberi", text: "Hello how are you doing!"},
+        {sender: "ubaidrmn", text: "Hello whatz upzzz ggg"},
+        {sender: "taha_zuberi", text: "Hello how are you doing!"},
+        {sender: "ubaidrmn", text: "Hello whatz upzzz ggg"},
+        {sender: "ubaidrmn", text: "Hello whatz upzzz ggg"},
+        {sender: "ubaidrmn", text: "Hello whatz upzzz ggg"},
+        {sender: "taha_zuberi", text: "Hello how are you doing!"},
+        {sender: "ubaidrmn", text: "Hello whatz upzzz ggg"},
+        {sender: "taha_zuberi", text: "Hello how are you doing!"},
+        {sender: "ubaidrmn", text: "Hello whatz upzzz ggg"},
+        {sender: "taha_zuberi", text: "Hello how are you doing!"},
+
+    ]
+
+    return <>
+        <div style={{
+            backgroundColor: "white",
+            padding: "50px",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+        }}>
+            <div style={{overflowY: "scroll", maxHeight: "500px", marginBottom: "30px"}}>
+                {messageHistory.map(message => 
+                    message.sender == username ? 
+                        <SentMessageBoxStyled align={"right"}>
+                            <div className="main-container"><div className="message-box">{message.text}</div></div>
+                        </SentMessageBoxStyled>
+                    : <ReceivedMessageBoxStyled>{message.text}</ReceivedMessageBoxStyled>
+                )}
+            </div>
+            <SendMessageInputStyled placeholder="Type a message" />
+        </div>
+    </>
+}
+
+export default ChatBox;
