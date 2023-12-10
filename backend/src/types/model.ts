77f39b105cbc6ee6da35230da0e3a7552b1ce027
ubaidrmn <rehmanubaid2003@gmail.com>
2023-12-11 02:09:53 +0500

    chore: update model types

--- backend/src/types/model.ts
+++ backend/src/types/model.ts
@@ -1,7 +1,11 @@
+import Message from '../models/Message'
 import Job from '../models/Job'
 import User from '../models/User'
+import Chat from '../models/Chat'
 
 export type Models = {
   Job: typeof Job
   User: typeof User
+  Message: typeof Message
+  Chat: typeof Chat
 }
