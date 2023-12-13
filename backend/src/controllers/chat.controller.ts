9cfabf733737d73959eeefc95fff9d91fa84dae8
ubaidrmn <rehmanubaid2003@gmail.com>
2023-12-13 18:57:56 +0500

    update chat controllers

--- backend/src/controllers/chat.controller.ts
+++ backend/src/controllers/chat.controller.ts
@@ -3,30 +3,37 @@
 // Utils
 import { generateController } from '../utils/generateController'
 import ErrorLogger from '../services/ErrorLogger'
-import { Op } from 'sequelize'
+import { Op, QueryTypes } from 'sequelize'
 
 // Model
 import Chat from '../models/Chat'
 import Message from '../models/Message'
 import ChatMember from '../models/ChatMember'
+import { db } from '../config/db'
 
 
 const connections = new Map()
 
 export const chatSignalingController = async (ws, req) => {
     const route = req.path
-    connections.set(route, ws)
-    m = Message.findAll()
+
+    if ( !connections.get(route) ) {
+      connections.set(route, ws);
+    } 
 
     ws.on('message', message => {
       message = JSON.parse(message)
-      const receiver_id = message['receiver_id'];
+      const receiver_id = message.receiver_id;
       const data = message.data
+  
       let receiver_connection = connections.get(`/signaling/${receiver_id}/.websocket`)
-      // if (receiver_connection == undefined) {
-      //   ws.send(JSON.stringify({"error": "user is not online"}))
-      // }
-      receiver_connection.send(JSON.stringify(data))
+
+      if (receiver_connection) {
+        ws.send(JSON.stringify({"success": "message sent to user!"}))
+        receiver_connection.send(JSON.stringify(data))
+      } else {
+        ws.send(JSON.stringify({"error": "user is not online :("}))
+      }
     })
 
 }
@@ -64,12 +71,19 @@ export const getChatsByUserId = generateController(
     try {
       const { user_id } = req.params
 
-      const chats = await ChatMember.findAll({
-        where: {
-          member_id: user_id ,
-        },
-      })
+      // const chats = await ChatMember.findAll({
+      //   where: {
+      //     member_id: user_id,
+      //   },
+      // })
 
+      const sql = `SELECT * FROM (SELECT * FROM chat_members a inner join
+        ( select member_id as recipient_member_id, chat_id from chat_members where 
+          member_id != '${user_id}' ) b on a.chat_id = b.chat_id WHERE 
+          member_id = '${user_id}') c inner join (select id, 
+          concat("firstName", ' ', "lastName") as recipient_name from users) d 
+          on c.recipient_member_id = d.id`
+      const chats = await db.query(sql, { type: QueryTypes.SELECT });
       return {
         message: 'Chats fetched successfully',
         payload: {
--- frontend/src/types/chat.ts
+++ frontend/src/types/chat.ts
@@ -1,4 +1,7 @@
 export type Chat = {
     chat_id: string;
     member_id: string;
+    recipient_name: string;
+    recipient_member_id: string;
+    id: string;
 }
