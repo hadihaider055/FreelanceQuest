cc5964e7fc3ef9d1189c6bb91e76e0fa51a68e69
haiderhadi055 <haiderhadi055@gmail.com>
2023-12-27 11:51:08 +0500

    fix: code structure

--- frontend/src/config/Paths.ts
+++ frontend/src/config/Paths.ts
@@ -3,5 +3,8 @@ export const Paths = {
     LOGIN: "/user/signin",
     SIGNUP: "/user/signup",
     METADATA: (email: string) => `/user/metadata?email=${email}`,
+    FETCH_USER_CHAT: (userId: string) => `/chat/user/${userId}`,
+    FETCH_CHAT_MESSAGES: (chatId: string) => `/chat/${chatId}/messages`,
+    SEND_MESSAGE: "/message",
   },
 };
--- frontend/src/store/thunks/chatThunk.ts
+++ frontend/src/store/thunks/chatThunk.ts
@@ -9,42 +9,49 @@ import axiosInstances from "@/config/axios";
 import Swal from "sweetalert2";
 import { setActiveChatMessages, setUserChats } from "../slices/chatSlice";
 
-export const fetchUserChats = createAsyncThunk(
-    "chat/fetchUserChats",
-    async (userId: string, { dispatch }) => {
-      try {
-        const chats = await axiosInstances.default.get(`/chat/user/${userId}/`);
-        dispatch(setUserChats(chats.data.payload.chats))
-        return chats;
-      } catch (e: any) {
-        console.log(e);
-        let errorMessage = e.message || "Failed to fetch chats";
-        if (e?.response?.data?.message) {
-          errorMessage = e.response.data.message;
-        }
+// Config
+import { Paths } from "@/config/Paths";
 
-        throw new Error(errorMessage);
+export const fetchUserChats = createAsyncThunk(
+  "chat/fetchUserChats",
+  async (userId: string, { dispatch }) => {
+    try {
+      const chats = await axiosInstances.default.get(
+        Paths.default.FETCH_USER_CHAT(userId)
+      );
+      dispatch(setUserChats(chats.data.payload.chats));
+      return chats;
+    } catch (e: any) {
+      console.log(e);
+      let errorMessage = e.message || "Failed to fetch chats";
+      if (e?.response?.data?.message) {
+        errorMessage = e.response.data.message;
       }
+
+      throw new Error(errorMessage);
     }
-  );
+  }
+);
 
 export const fetchChatMessages = createAsyncThunk(
-    "chat/fetchChatMessages",
-    async (chatId: string, { dispatch }) => {
-      try {
-        const chat = await axiosInstances.default.get(`/chat/${chatId}/messages/`);
-        dispatch(setActiveChatMessages(chat.data.payload.messages));
-        return chat;
-      } catch (e: any) {
-        console.log(e);
-        let errorMessage = e.message || "Failed to fetch chat messages";
-        if (e?.response?.data?.message) {
-          errorMessage = e.response.data.message;
-        }
-
-        throw new Error(errorMessage);
+  "chat/fetchChatMessages",
+  async (chatId: string, { dispatch }) => {
+    try {
+      const chat = await axiosInstances.default.get(
+        Paths.default.FETCH_CHAT_MESSAGES(chatId)
+      );
+      dispatch(setActiveChatMessages(chat.data.payload.messages));
+      return chat;
+    } catch (e: any) {
+      console.log(e);
+      let errorMessage = e.message || "Failed to fetch chat messages";
+      if (e?.response?.data?.message) {
+        errorMessage = e.response.data.message;
       }
+
+      throw new Error(errorMessage);
     }
+  }
 );
 
 // to permanently store the message on backend
@@ -53,7 +60,10 @@ export const postMessage = createAsyncThunk(
   "chat/postMessage",
   async (data, { dispatch }) => {
     try {
-      const message = await axiosInstances.default.post(`/message/`, JSON.stringify(data));
+      const message = await axiosInstances.default.post(
+        Paths.default.SEND_MESSAGE,
+        JSON.stringify(data)
+      );
       return message;
     } catch (e: any) {
       console.log(e);
