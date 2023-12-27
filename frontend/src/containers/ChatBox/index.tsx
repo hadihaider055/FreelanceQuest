0427ac84f821e433676a2230925ca8d02fb820ac
haiderhadi055 <haiderhadi055@gmail.com>
2023-12-27 11:59:08 +0500

    fix: code inconsistencies

--- frontend/src/containers/ChatBox/index.tsx
+++ frontend/src/containers/ChatBox/index.tsx
@@ -4,120 +4,144 @@
 import { fetchChatMessages } from "@/store/thunks/chatThunk";
 import { useAppDispatch, useAppSelector } from "@/utils/hooks/store";
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
-import { styled } from "styled-components"
+import React, { useEffect, useState } from "react";
+import { styled } from "styled-components";
 
-const SentMessageBoxStyled = styled.div<{align: string}>`
-    div.main-container {
-        max-width: 100%;
-        width: 100%;
-        display: flex;
-        flex-direction: row;
-        justify-content: ${props => props.align};
-    }
+const SentMessageBoxStyled = styled.div<{ align: string }>`
+  div.main-container {
+    max-width: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: ${(props) => props.align};
+  }
 
-    div.message-box {
-        background-color: rgb(34 197 94);
-        color: white;
-        max-width: 300px;
-        width: 100%;
-        border-radius: 10px;
-        padding: 15px;
-        margin-bottom: 15px;
-    }
+  div.message-box {
+    background-color: rgb(34 197 94);
+    color: white;
+    max-width: 300px;
+    width: 100%;
+    border-radius: 10px;
+    padding: 15px;
+    margin-bottom: 15px;
+  }
 `;
 
 const SendMessageInputStyled = styled.input`
-    border: 1px solid #d9d9d9;
-    max-width: 100%;
-    bottom: 30px;
-    width: 100%;
-    padding: 15px;
-    border-radius: 10px;
-    outline: none;
-    box-sizing: border-box;
-    height: 50px;
-    &::placeholder {
-        color: #9dafbd;
-    }
+  border: 1px solid #d9d9d9;
+  max-width: 100%;
+  bottom: 30px;
+  width: 100%;
+  padding: 15px;
+  border-radius: 10px;
+  outline: none;
+  box-sizing: border-box;
+  height: 50px;
+  &::placeholder {
+    color: #9dafbd;
+  }
 `;
 
 const ChatHistoryContainer = styled.div`
-    overflow-y: scroll;
-    max-height: 80vh;
-    margin-bottom: 30px;
-    @media (max-height: 690px) {
-        max-height: 70vh;
-    }
+  overflow-y: scroll;
+  max-height: 80vh;
+  margin-bottom: 30px;
+  @media (max-height: 690px) {
+    max-height: 70vh;
+  }
 `;
 
 const ReceivedMessageBoxStyled = styled.div`
-    background-color: #f2f2f2;
-    color: black;
-    max-width: 300px;
-    border-radius: 10px;
-    width: 100%;
-    padding: 15px;
-    box-sizing: border-box;
-    margin-bottom: 15px;
+  background-color: #f2f2f2;
+  color: black;
+  max-width: 300px;
+  border-radius: 10px;
+  width: 100%;
+  padding: 15px;
+  box-sizing: border-box;
+  margin-bottom: 15px;
 `;
 
 const ChatBox = (props) => {
+  const { sendMessage } = props;
 
-    const { sendMessage } = props;
+  const dispatch = useAppDispatch();
+  const activeChat = useAppSelector((state) => state.chat.activeChat);
+  const activeChatMessages = useAppSelector(
+    (state) => state.chat.activeChatMessages
+  );
+  const session = useSession();
 
-    const dispatch = useAppDispatch();
-    const activeChat = useAppSelector(state => state.chat.activeChat);
-    const activeChatMessages = useAppSelector(state => state.chat.activeChatMessages);
-    const session = useSession();
+  const scrollToBottomOfChat = () => {
+    try {
+      const messageHistoryContainer = document.getElementById(
+        "chat-history-container"
+      );
+      messageHistoryContainer.scrollTop = messageHistoryContainer.scrollHeight;
+    } catch (e) {}
+  };
 
-    const scrollToBottomOfChat = () => {
-        try {
-            const messageHistoryContainer = document.getElementById("chat-history-container")
-            messageHistoryContainer.scrollTop = messageHistoryContainer.scrollHeight;
-        } catch (e) {}
+  useEffect(() => {
+    scrollToBottomOfChat();
+  }, [activeChatMessages]);
+
+  useEffect(() => {
+    console.log(activeChat);
+    if (activeChat) {
+      dispatch(fetchChatMessages(activeChat.chat_id));
     }
+  }, [activeChat]);
 
-    useEffect(() => {
-        scrollToBottomOfChat();
-    }, [activeChatMessages])
-
-    useEffect(() => {
-        console.log(activeChat);
-        if (activeChat) {
-            dispatch(fetchChatMessages(activeChat.chat_id));
-        }
-    }, [activeChat])
-
-    return <>
-        { activeChat && 
-            <>
-            <div style={{
-                backgroundColor: "white",
-                padding: "50px",
-                height: "100%",
-                display: "flex",
-                flexDirection: "column",
-            }}>
-                <ChatHistoryContainer id="chat-history-container">
-                    {activeChatMessages && session.data && activeChatMessages.map(message => 
-                        message.sender_id == session?.data?.user.id ? 
-                            <SentMessageBoxStyled align={"right"}>
-                                <div className="main-container"><div className="message-box">{message.message}</div></div>
-                            </SentMessageBoxStyled>
-                        : <ReceivedMessageBoxStyled>{message.message}</ReceivedMessageBoxStyled>
-                    )}
-                </ChatHistoryContainer>
-                <SendMessageInputStyled onKeyDown={(e) => {
-                    if (e.key == 'Enter') {
-                        sendMessage(activeChat.recipient_member_id, e.target.value, activeChat.chat_id)
-                        e.target.value = "";
-                    }
-                }} placeholder="Type a message" />
-            </div>
-            </>
-        }
+  return (
+    <>
+      {activeChat && (
+        <>
+          <div
+            style={{
+              backgroundColor: "white",
+              padding: "50px",
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <ChatHistoryContainer id="chat-history-container">
+              {activeChatMessages &&
+                session.data &&
+                activeChatMessages.map((message, _id) => (
+                  <React.Fragment key={_id}>
+                    message.sender_id == session?.data?.user.id ? (
+                    <SentMessageBoxStyled align={"right"}>
+                      <div className="main-container">
+                        <div className="message-box">{message.message}</div>
+                      </div>
+                    </SentMessageBoxStyled>
+                    ) : (
+                    <ReceivedMessageBoxStyled>
+                      {message.message}
+                    </ReceivedMessageBoxStyled>
+                    )
+                  </React.Fragment>
+                ))}
+            </ChatHistoryContainer>
+            <SendMessageInputStyled
+              onKeyDown={(e) => {
+                if (e.key == "Enter") {
+                  sendMessage(
+                    activeChat.recipient_member_id,
+                    e.target.value,
+                    activeChat.chat_id
+                  );
+                  e.target.value = "";
+                }
+              }}
+              placeholder="Type a message"
+            />
+          </div>
+        </>
+      )}
     </>
-}
+  );
+};
 
 export default ChatBox;
--- frontend/src/containers/ChatList/index.tsx
+++ frontend/src/containers/ChatList/index.tsx
@@ -9,103 +9,130 @@ const InputStyled = styled.div`
   position: relative;
 
   .input-container {
-        width: 100%;
-        height: 100%;
-        border: 1px solid #d9d9d9;
-        border-radius: 10px;
-        overflow: hidden;
-        outline: none;
+    width: 100%;
+    height: 100%;
+    border: 1px solid #d9d9d9;
+    border-radius: 10px;
+    overflow: hidden;
+    outline: none;
 
     .input {
-        outline: none;
-        padding: 15px;
-        border: none;
-        width: 100%;
-        height: 100%;
+      outline: none;
+      padding: 15px;
+      border: none;
+      width: 100%;
+      height: 100%;
 
-        &::placeholder {
-            color: #9dafbd;
-        }
+      &::placeholder {
+        color: #9dafbd;
+      }
+    }
   }
 `;
 
 const StyledChatListRow = styled.div`
-    padding: 30px;
-    padding-top: 20px;
-    padding-bottom: 20px;
-    cursor: pointer;
-    transition: 0.25s;
-    &:hover {
-        background: var(--black60);
-        color: var(--white);
-    }
+  padding: 30px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  cursor: pointer;
+  transition: 0.25s;
+  &:hover {
+    background: var(--black60);
+    color: var(--white);
+  }
 `;
 
 const ChatList = (props) => {
+  const { connections } = props;
 
-    const { connections } = props;
+  const userChats = useAppSelector((state) => state.chat.chats);
+  const activeChat = useAppSelector((state) => state.chat.activeChat);
+  const dispatch = useAppDispatch();
 
-    const userChats = useAppSelector(state => state.chat.chats);
-    const activeChat = useAppSelector(state => state.chat.activeChat);
-    const dispatch = useAppDispatch();
+  // status: "online", displayPicture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1Fs8Arl_LnQwQ8ppF4IpZJ88JMXu4SHf7iFLcKQtUqg&s"
 
-    // status: "online", displayPicture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1Fs8Arl_LnQwQ8ppF4IpZJ88JMXu4SHf7iFLcKQtUqg&s"
-
-    return (
-        <div style={{
-            height: "100%",
-            maxHeight: "100%",
-            maxWidth: "100%",
-            width: "100%",
-            backgroundColor: "#f2f2f2",
-        }}>
-            <div style={{
-                padding: "30px",
-            }}>
-                <p style={{
-                    fontSize: "35px",
-                    fontWeight: "600",
-                    marginBottom: "30px",
-                }}>Chats</p>
-                <InputStyled>
-                    <div className="input-container">
-                        <input className="input" placeholder="Search" />
-                    </div>
-                </InputStyled>
+  return (
+    <div
+      style={{
+        height: "100%",
+        maxHeight: "100%",
+        maxWidth: "100%",
+        width: "100%",
+        backgroundColor: "#f2f2f2",
+      }}
+    >
+      <div
+        style={{
+          padding: "30px",
+        }}
+      >
+        <p
+          style={{
+            fontSize: "35px",
+            fontWeight: "600",
+            marginBottom: "30px",
+          }}
+        >
+          Chats
+        </p>
+        <InputStyled>
+          <div className="input-container">
+            <input className="input" placeholder="Search" />
+          </div>
+        </InputStyled>
+      </div>
+      {userChats &&
+        userChats.map((chat) => (
+          <StyledChatListRow
+            key={chat.chat_id}
+            onClick={(e) => {
+              if (
+                !activeChat ||
+                (activeChat && activeChat.chat_id != chat.chat_id)
+              ) {
+                dispatch(setActiveChat(chat));
+                dispatch(setActiveChatMessages(null));
+              }
+            }}
+            className="flex"
+          >
+            <img
+              style={{
+                width: "50px",
+                height: "50px",
+                marginRight: "20px",
+                borderRadius: "100%",
+              }}
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1Fs8Arl_LnQwQ8ppF4IpZJ88JMXu4SHf7iFLcKQtUqg&s"
+            />
+            <div className="flex-col">
+              <p className="text-xl" style={{ color: "var(--green-dark)" }}>
+                {chat.recipient_name}
+              </p>
+              <p className="text-sm">
+                {Object.keys(connections).includes(chat.recipient_member_id) &&
+                connections[chat.recipient_member_id]
+                  ? "online"
+                  : "offline"}
+              </p>
             </div>
-            {userChats && userChats.map(chat =>
-                <StyledChatListRow key={chat.chat_id} onClick={e => {
-                    if (!activeChat || (activeChat && activeChat.chat_id != chat.chat_id)) {
-                        dispatch(setActiveChat(chat));
-                        dispatch(setActiveChatMessages(null));
-                    }
-                }} className="flex">
-                    <img style={{
-                        width: "50px",
-                        height: "50px",
-                        marginRight: "20px",
-                        borderRadius: "100%"
-                    }} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1Fs8Arl_LnQwQ8ppF4IpZJ88JMXu4SHf7iFLcKQtUqg&s" />
-                    <div className="flex-col">
-                        <p className="text-xl" style={{color: "var(--green-dark)"}}>{chat.recipient_name}</p>
-                        <p className="text-sm">{Object.keys(connections).includes(chat.recipient_member_id) && connections[chat.recipient_member_id] ? "online" : "offline"}</p>
-                    </div>
-                </StyledChatListRow>
-            )}
+          </StyledChatListRow>
+        ))}
 
-
-            {!userChats || userChats.length <= 0 && 
-            <div style={{
-                padding: "30px",
-                paddingTop: "20px",
-                paddingBottom: "20px",
-            }}>
-                <p>You don't have any messages yet!</p>
-            </div>
-            }
-
-        </div>
-    );
-}
+      {!userChats ||
+        (userChats.length <= 0 && (
+          <div
+            style={{
+              padding: "30px",
+              paddingTop: "20px",
+              paddingBottom: "20px",
+            }}
+          >
+            <p>You don&apos;t have any messages yet!</p>
+          </div>
+        ))}
+    </div>
+  );
+};
 
 export default ChatList;
