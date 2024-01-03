0a46e62d09e1bdb7798b9897090d655c44d8ddf4
haiderhadi055 <haiderhadi055@gmail.com>
2024-01-03 18:28:46 +0500

    fix: build

--- frontend/src/containers/Proposal/ProposalDetailsContainer/index.tsx
+++ frontend/src/containers/Proposal/ProposalDetailsContainer/index.tsx
@@ -4,75 +4,86 @@ import { getProposalById } from "@/store/thunks/proposalThunk";
 import { useAppDispatch } from "@/utils/hooks/store";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { ProposalDetailsContainerStyled, ProposalInformationContainer } from "./styled";
+import {
+  ProposalDetailsContainerStyled,
+  ProposalInformationContainer,
+} from "./styled";
 import Container from "@/components/common/Container";
 import { SubmitProposalCategoryContainer } from "../SubmitProposalContainer/styled";
 import Button from "@/components/common/Button";
 import { FaArrowCircleLeft } from "react-icons/fa";
 
 const ProposalDetailsContainer: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const [proposal, setProposal] = useState(null);
+  const router = useRouter();
 
-    const dispatch = useAppDispatch();
-    const [proposal, setProposal] = useState(null);
-    const router = useRouter();
+  const fetchProposal = async (id) => {
+    const res = await dispatch(getProposalById(id));
+    return res;
+  };
 
-    const fetchProposal = async (id) => {
-        const res = await dispatch(getProposalById(id));
-        return res;
+  useEffect(() => {
+    const proposalId = router.query["id"];
+
+    if (proposalId) {
+      fetchProposal(proposalId).then((res) => setProposal(res.payload));
     }
+  }, [router.query]);
 
-    useEffect(() => {
-        const proposalId = router.query['id'];
+  return (
+    <>
+      <ProposalDetailsContainerStyled>
+        <Container>
+          <h1>Proposal Details</h1>
+          {proposal && (
+            <ProposalInformationContainer>
+              <h2>{proposal.Job.title}</h2>
+              <SubmitProposalCategoryContainer
+                style={{ marginTop: "20px", marginBottom: "20px" }}
+              >
+                {proposal.Job.skills.map((skill) => (
+                  <div key={skill}>{skill}</div>
+                ))}
+              </SubmitProposalCategoryContainer>
+              <p>{proposal.Job.description}</p>
 
-        if (proposalId) {
-            fetchProposal(proposalId)
-            .then(res => setProposal(res.payload));
-        }
+              <hr />
 
-    }, [router.query])
+              <h2>Your proposed terms</h2>
+              <p>Total price of project: {proposal.proposed_price}</p>
 
-    return <>
-        <ProposalDetailsContainerStyled>
-            <Container>
-                <h1>Proposal Details</h1>
-                {proposal &&
-                    <ProposalInformationContainer>
-                        <h2>{proposal.Job.title}</h2>
-                        <SubmitProposalCategoryContainer style={{ marginTop: "20px", marginBottom: "20px"}} >
-                            {proposal.Job.skills.map(skill => <div>{skill}</div>)}
-                        </SubmitProposalCategoryContainer>
-                        <p>{proposal.Job.description}</p>
+              <hr />
 
-                        <hr/>
-
-                        <h2>Your proposed terms</h2>
-                        <p>Total price of project: {proposal.proposed_price}</p>
-
-                        <hr/>
-
-                        <h2>Cover Letter</h2>
-                        <p>{proposal.cover_letter}</p>
-                    </ProposalInformationContainer>    
-                }
-                <div style={{ 
-                        maxWidth: "150px",
-                        whiteSpace: "nowrap",
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center" }}>
-                    <br/>
-                    <Button onClick={() => {
-                        router.push(`/proposals/`);
-                    }} variant="black">
-                        <span style={{ display: "inline-flex" }}>
-                            <FaArrowCircleLeft style={{ marginRight: "8px" }} /> Back
-                        </span>
-                    </Button>
-                </div>
-            </Container>
-
-        </ProposalDetailsContainerStyled>
+              <h2>Cover Letter</h2>
+              <p>{proposal.cover_letter}</p>
+            </ProposalInformationContainer>
+          )}
+          <div
+            style={{
+              maxWidth: "150px",
+              whiteSpace: "nowrap",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <br />
+            <Button
+              onClick={() => {
+                router.push(`/proposals/`);
+              }}
+              variant="black"
+            >
+              <span style={{ display: "inline-flex" }}>
+                <FaArrowCircleLeft style={{ marginRight: "8px" }} /> Back
+              </span>
+            </Button>
+          </div>
+        </Container>
+      </ProposalDetailsContainerStyled>
     </>
-}
+  );
+};
 
 export default ProposalDetailsContainer;
--- frontend/src/containers/Proposal/SubmitProposalContainer/index.tsx
+++ frontend/src/containers/Proposal/SubmitProposalContainer/index.tsx
@@ -1,7 +1,14 @@
 // @ts-nocheck
 
 import Container from "@/components/common/Container";
-import { SubmitProposalBidInput, SubmitProposalButton, SubmitProposalCategoryContainer, SubmitProposalCoverLetterInput, SubmitProposalSection, SubmitProposalStyled } from "./styled";
+import {
+  SubmitProposalBidInput,
+  SubmitProposalButton,
+  SubmitProposalCategoryContainer,
+  SubmitProposalCoverLetterInput,
+  SubmitProposalSection,
+  SubmitProposalStyled,
+} from "./styled";
 import { useEffect, useState } from "react";
 import { useAppDispatch } from "@/utils/hooks/store";
 import { useRouter } from "next/router";
@@ -11,104 +18,127 @@ import { useSearchParams } from "next/navigation";
 import { getJobByIdThunk } from "@/store/thunks/jobThunk";
 
 const SubmitProposalContainer: React.FC = () => {
-    const [coverLetter, setCoverLetter] = useState("");
-    const [bidAmount, setBidAmount] = useState("");
-    const router = useRouter();
-    const dispatch = useAppDispatch();
-    const session = useSession();
-    const params = useSearchParams();
-    const [job, setJob] = useState(null);
+  const [coverLetter, setCoverLetter] = useState("");
+  const [bidAmount, setBidAmount] = useState("");
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const session = useSession();
+  const params = useSearchParams();
+  const [job, setJob] = useState(null);
 
-    const fetchJob = async (id) => {
-        const res = await dispatch(getJobByIdThunk(id));
-        return res;
+  const fetchJob = async (id) => {
+    const res = await dispatch(getJobByIdThunk(id));
+    return res;
+  };
+
+  useEffect(() => {
+    const jobId = params.get("job_id");
+
+    if (jobId) {
+      fetchJob(jobId).then((res) => setJob(res.payload));
+    }
+  }, [params]);
+
+  const cancelAction = () => {
+    router.push("/");
+  };
+
+  const submitProposalAction = () => {
+    const jobId = params.get("job_id");
+
+    if (session.data && jobId) {
+      dispatch(
+        submitProposal({
+          job_id: jobId,
+          cover_letter: coverLetter,
+          proposed_price: bidAmount,
+          user_id: session.data.user.id,
+        })
+      );
     }
 
-    useEffect(() => {
-        const jobId = params.get('job_id');
+    router.push("/");
+  };
 
-        if (jobId) {
-            fetchJob(jobId)
-            .then(res => setJob(res.payload));
-        }
-
-    }, [params])
-
-    const cancelAction = () => {
-        router.push("/");
-    }
-
-    const submitProposalAction = () => {
-        const jobId = params.get('job_id');
-
-        if (session.data && jobId) {
-            dispatch(submitProposal({
-                job_id: jobId,
-                cover_letter: coverLetter,
-                proposed_price: bidAmount,
-                user_id: session.data.user.id
-            }))
-        }
-
-        router.push("/")
-    }
-
-    return <>
-        {job &&  
+  return (
+    <>
+      {job && (
         <SubmitProposalStyled>
-            <Container>
-                <br/><br/>
-                <h1>Submit a proposal</h1>
-                <br/>
+          <Container>
+            <br />
+            <br />
+            <h1>Submit a proposal</h1>
+            <br />
 
-                <SubmitProposalSection>
-                    <div className="submit-proposal-section-head">
-                        <h3>Job details</h3>
-                    </div>
-                    <div className="submit-proposal-section-body">
-                        <h3>{job.title}</h3>
-                        <br/>
-                        <SubmitProposalCategoryContainer>
-                            {job.skills && job.skills.map(skill => <div>{skill}</div>)}
-                        </SubmitProposalCategoryContainer>
-                        <br/>
-                        <p>{job.description}</p>
-                        <br/>
-                        <p><a href={`/jobs/${job.id}`}>View job posting</a></p>
-                    </div>
-                </SubmitProposalSection>
+            <SubmitProposalSection>
+              <div className="submit-proposal-section-head">
+                <h3>Job details</h3>
+              </div>
+              <div className="submit-proposal-section-body">
+                <h3>{job.title}</h3>
+                <br />
+                <SubmitProposalCategoryContainer>
+                  {job.skills &&
+                    job.skills.map((skill) => <div key={skill}>{skill}</div>)}
+                </SubmitProposalCategoryContainer>
+                <br />
+                <p>{job.description}</p>
+                <br />
+                <p>
+                  <a href={`/jobs/${job.id}`}>View job posting</a>
+                </p>
+              </div>
+            </SubmitProposalSection>
 
-                <br/>
+            <br />
 
-                <SubmitProposalSection>
-                    <div className="submit-proposal-section-head">
-                        <h3>What is the full amount you'd like to bid for this job?</h3>
-                    </div>
-                    <div className="submit-proposal-section-body">
-                        <SubmitProposalBidInput onChange={e=>setBidAmount(e.target.value)} type="number" placeholder="Amount" />
-                    </div>
-                </SubmitProposalSection>
+            <SubmitProposalSection>
+              <div className="submit-proposal-section-head">
+                <h3>
+                  What is the full amount you&apos;d like to bid for this job?
+                </h3>
+              </div>
+              <div className="submit-proposal-section-body">
+                <SubmitProposalBidInput
+                  onChange={(e) => setBidAmount(e.target.value)}
+                  type="number"
+                  placeholder="Amount"
+                />
+              </div>
+            </SubmitProposalSection>
 
-                <br/>
+            <br />
 
-                <SubmitProposalSection>
-                    <div className="submit-proposal-section-head">
-                        <h3>Cover letter</h3>
-                    </div>
-                    <div className="submit-proposal-section-body">
-                        <SubmitProposalCoverLetterInput onChange={e=>setCoverLetter(e.target.value)} placeholder="Write a cover letter for your proposal" />
-                    </div>
-                </SubmitProposalSection>
-                
-                <br/>
+            <SubmitProposalSection>
+              <div className="submit-proposal-section-head">
+                <h3>Cover letter</h3>
+              </div>
+              <div className="submit-proposal-section-body">
+                <SubmitProposalCoverLetterInput
+                  onChange={(e) => setCoverLetter(e.target.value)}
+                  placeholder="Write a cover letter for your proposal"
+                />
+              </div>
+            </SubmitProposalSection>
 
-                <SubmitProposalButton onClick={submitProposalAction}>Submit</SubmitProposalButton>
-                <SubmitProposalButton onClick={cancelAction} variant="dark">Cancel</SubmitProposalButton>
+            <br />
 
-                <br/><br/><br/><br/>
-            </Container>
-        </SubmitProposalStyled>}
+            <SubmitProposalButton onClick={submitProposalAction}>
+              Submit
+            </SubmitProposalButton>
+            <SubmitProposalButton onClick={cancelAction} variant="dark">
+              Cancel
+            </SubmitProposalButton>
+
+            <br />
+            <br />
+            <br />
+            <br />
+          </Container>
+        </SubmitProposalStyled>
+      )}
     </>
-}
+  );
+};
 
 export default SubmitProposalContainer;
